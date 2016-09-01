0395e5ad70aa18a0f59c7208f46eef9f6848416d
Dapo Oloruntola <Dapo Oloruntola>
2016-09-01 04:06:49 +0100

    first commit

--- Contracts/Views/MainPage.xaml.cs
+++ Contracts/Views/MainPage.xaml.cs
@@ -37,54 +37,6 @@ namespace Contracts.Views
             this.Frame.Navigate(typeof(CustomersInfo));
         }
 
-        private async void Save_the_existing_image_click(object sender, RoutedEventArgs e)
-        {
-            FileOpenPicker openPicker = new FileOpenPicker();
-            openPicker.ViewMode = PickerViewMode.Thumbnail;
-            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
-            openPicker.FileTypeFilter.Add(".jpg");
-            openPicker.FileTypeFilter.Add(".jpeg");
-            openPicker.FileTypeFilter.Add(".png");
-            // Launch file open picker and caller app is suspended and may be terminated if required 
-            openPicker.PickSingleFileAsync();
-
-
-
-
-            StorageFile file = await openPicker.PickSingleFileAsync();
-
-            if (file != null)
-            {
-                // Application now has read/write access to the picked file
-                textBlock.Text = "Picked photo: " + file.Name;
-
-               
-                    // Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
-                    CachedFileManager.DeferUpdates(file);
-                    // write to file
-                    await FileIO.WriteTextAsync(file, file.Name);
-                    // Let Windows know that we're finished changing the file so the other app can update the remote version of the file.
-                    // Completing updates may require Windows to ask for user input.
-                    FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
-                if (status == FileUpdateStatus.Complete)
-                {
-                    textBlock.Text = "File " + file.Name + " was saved.";
-                }
-                else
-                {
-                    textBlock.Text = "File " + file.Name + " couldn't be saved.";
-                }
-            }
-            else
-            {
-                textBlock.Text = "Operation cancelled.";
-            }
-
-
-
-
-
-        }
 
     }
 }
